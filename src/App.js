f7707d6a208244f8b6354befb4d57a9da52254a1
Tim Maupin <timmaupin@live.com>
2023-05-09 09:40:47 -0500

    set up links and routes for website

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<NavLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<AboutUs />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="login" element={<Login />} />
+      <Route path="logout" element={<Logout />} />
+    </Route>
+  )
+);
+
+
+
+
+
+
 
 
 function App() {
