bdfc8c799f25335d97ad50ff8ea74fefa002d8f3
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-09 11:27:14 -0500

    Footer component created, added to Home.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="footer__social">
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="footer__link">
+                        Facebook
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="footer__link">
+                        Twitter
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="footer__link">
+                        Instagram
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="footer__link">
+                        LinkedIn
+                    </a>
+                </div>
+                <p className="footer__contact">
+                    Email: info@example.com | Phone: (123) 456-7890
+                </p>
+                <p className="footer__copyright">
+                    &copy; Tricia Sawyer, Bryan Gonzalez, Tim Maupin
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Footer;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Footer from "./Footer";
 
 
 class Home extends React.Component {
 
-  
+
   render() {
     return (
       <>
         <div>WELCOME HOME</div>
-        
+        <Footer />
+
+
       </>
     );
   }
