590877038eb71707ebd1b1b115ac124b59f8b0af
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-10 12:40:48 -0500

    try to add programming languages used in the project and added each members about

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,13 +1,59 @@
 import React from "react";
+import "./css/about.css";
+// import Skills from '../Components/skills.json';
+import Card from 'react-bootstrap/Card';
 
 class AboutUs extends React.Component {
   render() {
     return (
       <>
-        <h1>About Us</h1>
-        <p>
-          Pie ice cream jelly beans liquorice bonbon toffee. Gingerbread sweet
-          </p>
+        <h1 className="about-title">About Us</h1>
+
+        <Card className="cards">
+          <section className="tricia-card">
+            <Card.Title>Tricia Sawyer</Card.Title>
+            <Card.Img
+              variant="top"
+              src="profile-images/tricia.png"
+              alt="Tricia's profile" />
+            <Card.Body>
+              <Card.Text>Some quick example text for T.</Card.Text>
+              {/* <Button variant="primary">Go somewhere</Button> */}
+            </Card.Body>
+          </section>
+
+          <section className="tim-card">
+            <Card.Title>Tim Maupin</Card.Title>
+            <Card.Img
+              variant="top"
+              src=""
+              alt="Tim's profile" />
+            <Card.Body>
+              <Card.Text>Some quick example text.</Card.Text>
+              {/* <Button variant="primary">Go somewhere</Button> */}
+            </Card.Body>
+          </section>
+
+          <section className="bryan-card">
+            <Card.Title>Bryan Gonazalez</Card.Title>
+            <Card.Img
+              variant="top"
+              src=""
+              alt="Bryan's profile" />
+            <Card.Body>
+              <Card.Text>Some quick example text.</Card.Text>
+              {/* <Button variant="primary">Go somewhere</Button> */}
+            </Card.Body>
+          </section>
+        </Card>
+
+
+        <section id="languages">
+          <h1 className="section-title">
+            <span>Programming Languages</span>
+          </h1>
+          {/* <ul className="list-inline mx-auto skill-icon">[Skills]</ul> */}
+        </section>
       </>
     );
   }
