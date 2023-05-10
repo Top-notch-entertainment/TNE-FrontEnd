3c9990fe0a7bb9809f5e886efd8b9db538155f70
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-10 17:03:18 -0500

    style navigation menu

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import { withAuth0 } from "@auth0/auth0-react";
 import Footer from './Footer';
+import './css/home.css';
 
 
 class Home extends React.Component {
@@ -18,10 +19,10 @@ class Home extends React.Component {
                             <NavLink to="logout">Logout</NavLink>
                         </>
                     ) : (
-                        <NavLink to="login">Login</NavLink>
+                        <NavLink className='nav-link' to="login">Login</NavLink>
                     )}
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="about">About Us</NavLink>
+                    <NavLink className='nav-link' to="about">About</NavLink>
+                    <NavLink className='nav-link' to="/">Home</NavLink>
                 </nav>
                 <h1 className='header__title'>Top Notch Entertainment</h1>
             </header>
