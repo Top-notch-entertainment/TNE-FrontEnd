acf2334aa72acf5913079e5b8fea831d9191576d
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-10 14:11:50 -0500

    carousel added to Profile page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -42,8 +43,45 @@ const Profile = () => {
           </Form.Group>
           <Button variant="primary" type="submit">Search</Button>
         </Form>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="http://via.placeholder.com/640x360"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="http://via.placeholder.com/640x360"
+              alt="Second slide"
+            />
 
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="http://via.placeholder.com/640x360"
+              alt="Third slide"
+            />
 
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
         </>
     )
   );
