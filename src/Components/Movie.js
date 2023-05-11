5a416022c3e8c66782785bcf70dbad5f63438a5d
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-11 13:07:59 -0500

    create movie js

---
+++ src/Components/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Movie extends React.Component {
+
+    render() {
+
+        return (
+            <>
+            
+            
+            
+            </>
+       )
+    }
+}
